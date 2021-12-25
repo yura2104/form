43f336568db78d9c0f3c096aa15bbf83b10ff9a1
Yuriy Lukanin <yura-arh@yandex.ru>
2021-12-25 21:35:56 +0300

    Запись данных в таблицу, запрос на получение данных из таблицы php

---
+++ app/db/db.php
@@ -0,0 +1,63 @@
+<?php
+
+require 'connect.php';
+
+// тестовая функция
+function tt($value){
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+}
+
+// обработка ошибок
+function dbCheckError($query){
+    $errInfo = $query->errorInfo();
+    if($errInfo[0] !== PDO::ERR_NONE){
+        echo $errInfo[2];
+        exit();
+    }
+    return true;
+}
+
+// Запрос на получение данных одной таблицы
+function selectAll($table){
+    global $pdo;
+    $sql = "SELECT Сообщение FROM $table";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+
+    dbCheckError($query);
+    return $query->fetch();
+
+}
+
+// Запись данных в таблицу
+
+function insert($table, $params ){
+    global $pdo;
+    $i=0;
+    $coll = '';
+    $mask = '';
+    foreach ($params as $key => $value){
+        if($i===0){
+            $coll = $coll . "$key";
+            $mask = $mask . "'$value'"  ;
+        }else{
+            $coll = $coll . ",$key";
+            $mask = $mask . ",'$value'" ;
+        }
+        $i++;
+    }
+    $sql = "INSERT INTO $table ($coll) VALUES ($mask)";
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+    dbCheckError($query);
+}
+$arrData = [
+    'ФИО' => 'Gyn А.Л.',
+    'email' => 'fdfsd@gmail.com',
+    'Сообщение' => 'Push'
+];
+
+
+insert('users', $arrData);
